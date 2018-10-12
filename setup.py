19f1d1b841497b3f9ee2babfe0d18d1bdcefd56e
Christopher Lee <lee@foldmountain.com>
2018-10-12 13:58:48 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='deepmars',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Mars crater identification',
+    author='Christopher Lee',
+    license='MIT',
+)
